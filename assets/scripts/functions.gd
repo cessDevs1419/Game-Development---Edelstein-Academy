44016d04d5dc9ba84d87be66d8133ed05a474ea7
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-01-18 21:12:44 +0800

    status_bar_ui + character_selection
    - both created as a component
    - work dynamically
    - just need to redesign
    
    warn
    - still needs to adjust bug on character selection button

---
+++ assets/scripts/functions.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	pass
+
+func update_avatar(
+		avatars: Node, 
+		avatar_path: Image,
+		scaleX: float,
+		scaleY: float,
+		positionX: float,
+		positionY: float,
+	):
+	avatars.texture = ImageTexture.create_from_image(avatar_path)
+	avatars.scale.x = scaleX
+	avatars.scale.y = scaleY
+	avatars.position.x = positionX
+	avatars.position.y = positionY
+	
+func update_contents(namenode: Node, nameValue:String, lvlcountnode: Node, lvlcountValue:String):
+	namenode.text = nameValue
+	lvlcountnode.text = lvlcountValue
