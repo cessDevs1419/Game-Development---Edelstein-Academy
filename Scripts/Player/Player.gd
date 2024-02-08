c34e582f67498cab4fcf7d67c92e0b3efbfa5e55
Mark Angelo Dayrit <markangelodayrit00@gmail.com>
2024-02-08 21:51:51 +0800

    Added selected unit indicator and added more signals to better understand the player movement

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,15 +1,24 @@
 extends CharacterBody2D
 
-signal mouse_clicked(position)
+signal unit_selected(position)
+signal unit_moved(original_position, target_position)
 var isSelected: bool = false;
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("LeftClick"):
-		# global_position = Vector2($"../SandboxTileMap".selectedTile) # This will move the player
+	if Input.is_action_just_pressed("LeftClick") and isSelected:
+		var target_position = Vector2($"../SandboxTileMap".selectedTile)
+		var current_position = global_position
+		
+		if target_position != current_position:
+			isSelected = false;
+			emit_signal("unit_moved", current_position, target_position)
+		
+		global_position = target_position # this will move the character
+		
 		pass
 
 
 func _on_control_gui_input(event):
 	if event.is_action_pressed("LeftClick"):
-		isSelected = !isSelected
-		emit_signal("mouse_clicked", global_position)
+		isSelected = true
+		emit_signal("unit_selected", global_position)
