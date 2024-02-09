773d31427f31ea3b2e1b8eeeace38e4713e64547
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-02-09 12:29:44 +0800

    updating responsiveness

--- Assets/Components/Overview_Panel/Overview_Panel.gd
+++ Assets/Components/Overview_Panel/Overview_Panel.gd
@@ -6,29 +6,29 @@ var Itemlist
 
 func _ready():
 	Objectpanel = get_node("Objective")
-	#Characterpanel = get_node("Objective")
+	Characterpanel = get_node("Characterpanel")
 	Itemlist = get_node("Objective/Panel/ItemList")
 	add_item('kwan')
 
 func _process(delta):
 	pass
 
-func show_panel():
-	Objectpanel.position.x = 20
-	pass
-
-func hide_panel():
-	Objectpanel.position.x = -260
-	pass
-
-
-func _on_texture_button_toggled(toggled_on):
+func _objective_button_toggled(toggled_on):
 	if(toggled_on):
-		hide_panel()
+		Objectpanel.position.x = -260
 	else:
-		show_panel()
+		Objectpanel.position.x = 20
 	pass 
 
 func add_item(data: String):
 	Itemlist.add_item('• '+data, null, false)
 	pass
+
+
+func _character_button_toggled(toggled_on):
+	if(toggled_on):
+		Characterpanel.position.x = 1075
+	else:
+		Characterpanel.position.x = 800
+	pass 
+	pass 
